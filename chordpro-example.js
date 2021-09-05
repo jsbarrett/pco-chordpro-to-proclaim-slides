7846c1ad35f87092149e49bf6a06d9316126e719
Jacob Barrett <jsbarrett88@gmail.com>
2021-09-05 00:23:39 -0500

    Initial commit
    Getting general parsing setup and ability to download files
    Still need to integrate with Planning Center
    to dynamically pull the songs each week

---
+++ chordpro-example.js
@@ -0,0 +1,24 @@
+const chordchart = `INTRO
+	D            Em    D/F#     G             D/F#        Bm        A
+
+VERSE 1
+	How d[D]eep the Father's [Em]love  [D/F#] for     [G]us:: how [D/F#]vast beyond all [Asus]mea  -  [A]sure
+	That [D]He should give His [Em]on  -  [D/F#]ly      S[G]on:: to [D/F#]make a wretch His tr[A]ea - s[D]ure
+	How gr[D]eat the pain of s[Em]ea - [D/F#]ring l[G]oss:: the [D/F#]Father turns His [Bm]face aw[A]ay
+	As w[D]ounds which mar the [Em]Cho - [D/F#]sen     [G]One:: bring [D/F#]many sons to g[A]lo - r[D]y     [Dsus]     [D]    [Dsus]
+
+VERSE 2
+	Beh[D]old the Man u[Em]pon [D/F#]  a    cr[G]oss:: my [D/F#]sin upon His [Asus]shou - ld[A]ers
+	Ash[D]amed I hear my [Em]mo - [D/F#]cking v[G]oice:: call [D/F#]out among the s[A]coff[D]ers
+	It w[D]as my sin that [Em]held [D/F#]Him th[G]ere:: un[D/F#]til it was ac[Bm]compl[A]ished
+	His d[D]ying breath has [Em]brought[D/F#] me    l[G]ife:: I [D/F#]know that it is f[A]i - n[D]ished     [Dsus]     [D]    [Dsus]
+
+VERSE 3
+	I w[D]ill not boast in[Em] a  - [D/F#] ny - th[G]ing:: no [D/F#]gifts no power no [Asus]wi  -  sd[A]om
+	But [D]I will boast in[Em] Je -[D/F#] sus Chr[G]ist:: His [D/F#]death and resurr[A]ect[D]ion
+	Why sh[D]ould I gain from [Em]His[D/F#] re - w[G]ard:: I [D/F#]cannot give an [Bm]answ[A]er
+	But th[D]is I know with[Em] all[D/F#] my h[G]eart:: His w[D/F#]ounds have paid my r[A]ans[D]om     [Dsus]
+
+TAG
+	Why sh[D]ould I gain from [Em]His[D/F#] re - w[G]ard:: I [D/F#]cannot give an [Bm]answ[A]er
+	But th[D]is I know with[Em] all[D/F#] my h[G]eart:: His w[D/F#]ounds have paid my r[A]ans[D]om`
---
+++ index.js
@@ -0,0 +1,60 @@
+const consoleLog = x => { console.log(x); return x }
+
+const groupInTwos = lyrics => {
+  const grouped = [[]]
+
+  for (let i = 0; i < lyrics.length; i += 1) {
+    const lastGrouping = grouped[grouped.length - 1]
+    if (lastGrouping.length < 2) {
+      lastGrouping.push(lyrics[i])
+    } else {
+      grouped.push([lyrics[i]])
+    }
+  }
+
+  return grouped
+}
+
+const parseChordChart = chordchart => {
+  return chordchart
+    .replace(/\[.*?\]/g, '')
+    .replace(/\t/g, '')
+    .split('\n\n')
+    .map(x => {
+      const [section, ...lyrics] = x
+        .replace(/::::/g, '\n')
+        .replace(/::/g, '\n')
+        .split('\n')
+        .map(x => x.trim())
+      return {
+        section,
+        lyrics
+      }
+    })
+    .filter(x => x.section !== 'INTRO')
+    .map(x => {
+      const pairs = groupInTwos(x.lyrics)
+        .map(x => x.join('\n'))
+        .join('\n\n')
+
+      return `${x.section}\n${pairs}`
+    })
+    .join('\n\n')
+    .replace(/ +/g, ' ')
+    .replace(/ - /g, '')
+}
+
+const download = (name, data) => {
+  const anchorElement = document.createElement('a')
+  anchorElement.setAttribute('href', 'data:text/plan;charset=utf8,' + encodeURIComponent(data))
+  anchorElement.setAttribute('download', name)
+  document.body.appendChild(anchorElement)
+  anchorElement.click()
+  document.body.removeChild(anchorElement)
+}
+
+output.value = parseChordChart(chordchart)
+
+downloadbtn.addEventListener('click', () => {
+  download('How Deep The Father\'s Love For Us.txt', parseChordChart(chordchart))
+})
